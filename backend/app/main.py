7096a40fc18e7560e55f054863e1b555980ec9ab
Dany Khardaji <danykhardaji@protonmail.com>
2026-08-18 16:29:25 -0400

    Render stand markers from the API on the map

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
-origins = ["http://localhost:5500"]
+origins = ["http://localhost:5500", "http://127.0.0.1:5500"]
 
 app.add_middleware(
     CORSMiddleware,
